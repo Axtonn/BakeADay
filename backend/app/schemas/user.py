c763113c8706af7047b1427e9c8c5d4ffd6ce422
Axtonn <acah2508@uni.sydney.edu.au>
2025-12-12 12:18:59 +1100

    user field changes for clerk and other fields like names and images and admin fields

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,14 +1,20 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class UserBase(BaseModel):
+    clerk_id: str
     email: str
+    phone: Optional[str] = None
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    image_url: Optional[str] = None
+    is_admin: bool = False
 
 class UserCreate(UserBase):
-    password: str
+    pass
 
 class User(UserBase):
     id: int
-    is_admin: int
 
     class Config:
         orm_mode = True
