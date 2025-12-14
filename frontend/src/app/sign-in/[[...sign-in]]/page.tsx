fc2c85c2110a38077e8b8531fbb66e4c5311a9f3
Axtonn <acah2508@uni.sydney.edu.au>
2025-12-14 18:27:05 +1100

    sign in page modified

--- frontend/src/app/sign-in/[[...sign-in]]/page.tsx
+++ frontend/src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,26 @@
-import { SignIn } from "@clerk/nextjs";
+import { SignIn, useUser } from "@clerk/nextjs";
+import { useSearchParams, useRouter } from "next/navigation";
 
 export default function Page() {
+  const search = useSearchParams();
+  const router = useRouter();
+  const { isSignedIn } = useUser();
+  const redirect = search?.get("redirect_url") || "/";
+
+  if (isSignedIn) {
+    router.push(redirect);
+    return null;
+  }
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-pink-50">
-      <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+      <SignIn
+        path="/sign-in"
+        routing="path"
+        signUpUrl={`/sign-up?redirect_url=${encodeURIComponent(redirect)}`}
+        afterSignInUrl={redirect}
+        afterSignUpUrl={redirect}
+      />
     </div>
   );
 }
